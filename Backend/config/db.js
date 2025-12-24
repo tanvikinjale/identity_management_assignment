49d2e768b79b8bfcfd4b45de961fcc23720aad7c
Tanvi Kinjale <79430665+tanvikinjale@users.noreply.github.com>
2025-12-24 16:01:03 +0530

    Update database connection to use environment variables

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'tanvi28', 
-  database: 'user_management'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 }).promise() // to use async await instead of callback
 
 // const [result] = connection.query("SELECT * FROM login");
